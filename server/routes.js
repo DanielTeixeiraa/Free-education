b1d4cae3e5b70e29c4a29521c6b3ecfccb79d04f
Daniel <Daniel.teixeirari@gmail.com>
2020-06-09 22:39:24 -0300

    page of theacher

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const routes = express.Router();
 
 routes.get('/', function(req,res){
-  return res.redirect("/teachers/index")
+  return res.redirect("/teachers")
 })
 
 routes.get('/teachers', function(req,res){
